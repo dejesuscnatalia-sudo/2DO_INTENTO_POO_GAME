a6457c3512a1ea760110933b1bcc23d99502e3a6
natha <natha@PATO_PAN>
2026-03-24 21:43:11 -0700

    Agregar archivos de proyecto.

---
+++ 2DO_INTENTO_POO_GAME/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2DO_INTENTO_POO_GAME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
